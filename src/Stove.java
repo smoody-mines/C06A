39cfca0f55060d2a0fa18fc64957b60222c833d8
James Slopey <slopey@mines.edu>
2024-09-13 15:22:57 -0600

    Adjusted timePassing durations in main

--- src/Stove.java
+++ src/Stove.java
@@ -101,27 +101,28 @@ public class Stove {
 		Stove stove = new Stove();
 		
 		System.out.println("Beginning stove state ");
-		// trun the burners up
 		stove.displayStove();
+		
+		// trun the burners up
+		
 		stove.turnBurnersUp();
 		stove.timePassing(6);
 		System.out.println("\nStove after burners turned up ");
+		stove.displayStove();
 		
 		// adjust the burners
-		stove.displayStove();
 		stove.adjustBurners();
-		stove.timePassing(2);
+		stove.timePassing(3);
 		System.out.println("\nStove after burners adjusted ");
+		stove.displayStove();
 		
 		// some more adjustment test
-		stove.displayStove();
 		stove.moreBurnerAdjustments();
 		stove.timePassing(1);
-		
-		// Ensure temperatures don't update till time has passed
 		System.out.println("\nStove waiting for time to elapse ");
 		stove.displayStove();
-		stove.timePassing(1);
+		// Ensure temperatures don't update till time has passed
+		stove.timePassing(2);
 		System.out.println("\nStove in final state ");
 		stove.displayStove();	
 	}
